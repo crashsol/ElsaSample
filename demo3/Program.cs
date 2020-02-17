015c10934d8dd9c65ebc2f8c455495e8151b3891
唐林 <47147551@qq.com>
2020-02-17 13:52:29 +0800

    demo3

---
+++ demo3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Elsa.Activities.Console.Extensions;
+using Elsa.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace demo3
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var services = new ServiceCollection()
+                .AddElsa()
+                .AddConsoleActivities()
+                .BuildServiceProvider();
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
