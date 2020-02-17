4b75d18cc87c2770308f414eb5ccdcfedc021390
唐林 <47147551@qq.com>
2020-02-17 13:37:53 +0800

    demo2

---
+++ demo2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Elsa.Services;
+using Elsa.Activities.Console.Extensions;
+using Elsa.Activities.Console.Activities;
+using Elsa.Expressions;
+
+namespace demo2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var services = new ServiceCollection()
+                .AddElsa()
+                .AddConsoleActivities()
+                .BuildServiceProvider();
+            //定义工作流程
+            var workflowBuilderFactory = services.GetRequiredService<Func<IWorkflowBuilder>>();
+
+            var workBuilder = workflowBuilderFactory();
+            var workFlowDefinition = workBuilder
+                .StartWith<WriteLine>(x => x.TextExpression = new LiteralExpression("Start on"))
+                .Then<WriteLine>(x => x.TextExpression = new LiteralExpression("End"))
+                .Build();
+            //开始流程
+            var invoker = services.GetService<IWorkflowInvoker>();
+            await invoker.StartAsync(workFlowDefinition);
+            Console.ReadLine();
+        }
+    }
+}
