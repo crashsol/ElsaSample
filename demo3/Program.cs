c92b867de3fe7ff2101b18afa51eed55c57f4cf9
唐林 <47147551@qq.com>
2020-02-18 15:11:38 +0800

    学习自定义workflow

--- demo3/Program.cs
+++ demo3/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Elsa;
+using Elsa.Activities.Console.Activities;
 using Elsa.Activities.Console.Extensions;
+using Elsa.Expressions;
+using Elsa.Models;
 using Elsa.Services;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,8 +19,27 @@ namespace demo3
                 .AddConsoleActivities()
                 .BuildServiceProvider();
 
+            //Define a workflow as data so we can store it in( file ,database,)
+            var workflowDefinition = new WorkflowDefinitionVersion
+            {
+                Activities = new[]
+                {
+                    new ActivityDefinition<WriteLine>("activity-1",new { TextExpression = new LiteralExpression("Hello World!")}),
+                    new ActivityDefinition<WriteLine>("activity-2", new { TextExpression = new LiteralExpression("Goodbye cruel world...")})
+                },
+                Connections = new[]
+                {
+                    new ConnectionDefinition("activity-1","activity-2",OutcomeNames.Done)
+                }
 
-            Console.WriteLine("Hello World!");
+            };
+
+            var invoker = services.GetService<IWorkflowInvoker>();
+            await invoker.StartAsync(workflowDefinition);
+            Console.ReadLine();
+            
+
+        
         }
     }
 }
