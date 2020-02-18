b2660f475e4ffc326b5081b8f6007841cd599971
唐林 <47147551@qq.com>
2020-02-18 17:21:41 +0800

    添加学习Elsa使用 Http Activity

---
+++ demo6/HelloWorldWorkflow.cs
@@ -0,0 +1,34 @@
+﻿using Elsa.Activities.Http.Activities;
+using Elsa.Expressions;
+using Elsa.Services;
+using Elsa.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace demo6
+{
+    /// <summary>
+    /// 定义一个请求Activity
+    /// </summary>
+    public class HelloWorldWorkflow : IWorkflow
+    {
+        public void Build(IWorkflowBuilder builder)
+        {
+            builder.StartWith<ReceiveHttpRequest>(
+                activity => activity.Path = new Uri("/hello-world", UriKind.Relative)
+                ).Then<WriteHttpResponse>(
+
+                   activity =>
+                   {
+                       activity.Content = new LiteralExpression("<h1>Elsa say Hi</h1>");
+                       activity.ContentType = "text/html";
+                       activity.StatusCode = System.Net.HttpStatusCode.OK;
+                       activity.ResponseHeaders = new LiteralExpression("X-Powered-By=Elsa Workflows");
+                   }
+                );
+        }
+    }
+}
---
+++ demo6/Startup.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Elsa.Activities.Http.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace demo6
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //注入Elsa Http 请求流程
+            services.AddElsa()
+                .AddHttpActivities()
+                .AddWorkflow<HelloWorldWorkflow>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseHttpActivities();
+            app.UseWelcomePage();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
